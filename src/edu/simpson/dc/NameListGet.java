be327328802b7dc8e635f1f3ee6e3d2436fe01b8
DC Scott <dc.scott@my.simpson.edu>
2019-02-19 08:07:58 -0600

    "Lab 4 Final Update"ZZ

--- src/edu/simpson/dc/NameListGet.java
+++ src/edu/simpson/dc/NameListGet.java
@@ -2,6 +2,8 @@ package edu.simpson.dc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import com.google.gson.Gson;
 
 
 public class NameListGet extends javax.servlet.http.HttpServlet {
@@ -10,10 +12,18 @@ public class NameListGet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         // Type of output (HTML, JSON, image, whatever
-        response.setContentType("text/plain");
+        response.setContentType("application/json");
         // Get an object that can write to the network
         PrintWriter out = response.getWriter();
-        // Write to the network
-        out.print("Hello");
+
+        List<Person> peopleList = PersonDAO.getPeople();
+        Gson gson = new Gson();
+
+        // Go ahead and turn peopleList into a JSON string
+        String json = gson.toJson(peopleList);
+
+        // Write out that string
+        out.println(json);
+
     }
 }
--- web/js/name_list.js
+++ web/js/name_list.js
@@ -1,1 +1,59 @@
-console.log("Hi, this is a test.");
+//console.log("Hi, this is a test.");
+
+// Main Javascript File
+
+function updateTable() {
+
+
+// Define a URL
+    var url = "api/name_list_get";
+
+    $.getJSON(url, null, function (json_result) {
+        // json_result is an object. You can set a breakpoint, or print
+        // it to see the fields. Specifically, it is an array of objects.
+        // Here we loop the array and print the first name.
+        for (var i = 0; i < json_result.length; i++) {
+            // console.log(json_result[i].first);
+            // console.log(json_result[i].last);
+            // console.log(json_result[i].phone);
+            // console.log(json_result[i].email);
+            // console.log(json_result[i].birthday);
+            // console.log(json_result[i].id);
+
+            $('#datatable tr:first').after('<tr>' +
+                '<td>' + json_result[i].id + '</td>' +
+                '<td>' + json_result[i].first + '</td>' +
+                '<td>' + json_result[i].last + '</td>' +
+                '<td>' + json_result[i].email + '</td>' +
+                '<td>' + json_result[i].phone + '</td>' +
+                '<td>' + json_result[i].birthday + '</td>'
+                + '</tr>');
+        }
+    })
+}
+
+// Call your code.
+updateTable();
+
+// Define a function that will automatically be called when
+// our request is done.
+// function my_callback(json_result) {
+//     console.log("Done");
+// }
+//
+//
+// // Start a web call. Specify:
+// // URL
+// // Data to pass (nothing in this case)
+// // Function to call when we are done
+// $.getJSON(url, null, my_callback);
+//
+// var formButton1 = $('#button1');
+// formButton1.on("click", updateTable);
+//
+//
+//
+//     }
+//
+//
+// );
